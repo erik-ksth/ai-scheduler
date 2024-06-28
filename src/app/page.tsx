58608a37cbbadf4ee50468122abc908f9e1ab37b
Erik Hein <erikhein@Eriks-MacBook-Pro.local>
2024-06-28 02:11:37 -0700

    update: change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,59 +54,61 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <h1 className="text-center text-4xl font-bold my-10">AI SCHEDULER</h1>
-      <div className="flex min-h-screen flex-col md:flex-row gap-4 items-center p-4 md:p-10">
-        <div className=" md:w-1/2 md:h-screen">
-          <form onSubmit={handleSubmit} className="text-center">
-            <input
-              type="text"
-              className="text-black"
-              placeholder="Enter a task"
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <div className="flex flex-col md:px-5 md:py-3 md:my-4">
-            {tasks.map((task, index) => (
-              <div key={index} className="flex justify-between items-center">
-                <div>{task}</div>
-                <FaRegTrashCan
-                  className="my-3 cursor-pointer"
-                  onClick={() => handleDelete(index)}
-                />
-              </div>
-            ))}
+    <>
+      <main>
+        <h1 className="text-center text-4xl font-bold my-10">AI SCHEDULER</h1>
+        <div className="flex min-h-screen flex-col md:flex-row gap-4 items-center p-4 md:p-10">
+          <div className=" md:w-1/2 md:h-screen">
+            <form onSubmit={handleSubmit} className="text-center">
+              <input
+                type="text"
+                className="text-black"
+                placeholder="Enter a task"
+              />
+              <button type="submit">Submit</button>
+            </form>
+            <div className="flex flex-col md:px-5 md:py-3 md:my-4">
+              {tasks.map((task, index) => (
+                <div key={index} className="flex justify-between items-center">
+                  <div>{task}</div>
+                  <FaRegTrashCan
+                    className="my-3 cursor-pointer"
+                    onClick={() => handleDelete(index)}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-end">
+              <button onClick={handleClearAll}>Clear All</button>
+            </div>
+            <div className="text-center">
+              {!loading ? (
+                <button onClick={handleCreateSchedule}>Create Schedule</button>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
-          <div className="flex justify-end">
-            <button onClick={handleClearAll}>Clear All</button>
-          </div>
-          <div className="text-center">
-            {!loading ? (
-              <button onClick={handleCreateSchedule}>Create Schedule</button>
-            ) : (
-              <></>
-            )}
+          <div className="md:w-1/2">
+            <div className="flex items-start justify-center w-full px-10">
+              {!response && !loading ? (
+                <div className="text-center">
+                  <p>No schedule created yet</p>
+                </div>
+              ) : (
+                <></>
+              )}
+              {loading ? (
+                <div className="text-center">
+                  <l-helix size="45" speed="2.5" color="white"></l-helix>
+                </div>
+              ) : (
+                <pre className="text-wrap font-sans">{response}</pre>
+              )}
+            </div>
           </div>
         </div>
-        <div className="md:w-1/2">
-          <div className="flex items-start justify-center w-full px-10">
-            {!response && !loading ? (
-              <div className="text-center">
-                <p>No schedule created yet</p>
-              </div>
-            ) : (
-              <></>
-            )}
-            {loading ? (
-              <div className="text-center">
-                <l-helix size="45" speed="2.5" color="white"></l-helix>
-              </div>
-            ) : (
-              <pre className="text-wrap font-sans">{response}</pre>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
